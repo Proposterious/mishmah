1f1abcb5841f8d81ece80af06db8cf009f2931c8
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-12-27 09:48:15 -0600

    Migrate to Astro framework (replace Nextjs)

--- mishmah/app/_components/Navbar.tsx
+++ mishmah/app/_components/Navbar.tsx
@@ -1,19 +1,38 @@
+import Link from "next/link";
 import Image from "next/image";
 
-import amor from "../../public/amor_amor.jpg";
+import mdaSquare from "../../public/assets/edits/multicolored_dreams_author_square.jpg";
+
+
 
 function Navbar() {
+
     return (
-        <nav id="main-nav" className="flex flex-row justify-center space-x-4 py-6 | bg-zinc-900">
-            <Image src={amor}
-            width={200} height={20} 
+        <header className="flex flex-row justify-start py-2 | bg-zinc-900">
+            <Image src={mdaSquare}
+            width={76} height={20}
+            className="ml-12" 
             alt="placeholder-logo" />
 
-            <div className="text-slate-200 text-xl">Home</div>
-            <div className="text-slate-200 text-xl">About</div>
-            <div className="text-slate-200 text-xl">Works</div>
-            <div className="text-slate-200 text-xl">Blog</div>
-        </nav>
+            <nav id="main-nav" className="pr-36 | flex flex-row mx-auto self-center space-x-6">
+                <Link href="/" className="text-slate-200 text-xl">
+                    Home
+                </Link>
+
+                <Link href="/about" className="text-slate-200 text-xl">
+                    About
+                </Link>
+
+                <Link href="/works" className="text-slate-200 text-xl">
+                    Works
+                </Link>
+
+                <Link href="/blog" className="text-slate-200 text-xl">
+                    Blog
+                </Link>
+            </nav>
+
+        </header>
      );
 }
 
